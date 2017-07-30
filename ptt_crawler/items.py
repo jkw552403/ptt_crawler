df1ce5152223b6d3fde67890ac518a0861a4cd20
mcko <jkw552403@gmail.com>
2017-07-30 19:05:56 +0800

    Add PostItem and CommentItem

--- ptt_crawler/items.py
+++ ptt_crawler/items.py
@@ -8,7 +8,24 @@
 import scrapy
 
 
-class PttCrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class PostItem(scrapy.Item):
+    post_id = scrapy.Field()
+    title = scrapy.Field()
+    author_id = scrapy.Field()
+    board = scrapy.Field()
+    content = scrapy.Field()
+    datetime = scrapy.Field()
+    ip = scrapy.Field()
+    url = scrapy.Field()
+    external_urls = scrapy.Field()
+    source_hash = scrapy.Field()  # to check if there is any update in this post
+
+
+class CommentItem(scrapy.Item):
+    post_id = scrapy.Field()
+    user_id = scrapy.Field()
+    user_ip = scrapy.Field()
+    datetime = scrapy.Field()
+    content = scrapy.Field()
+    tag = scrapy.Field()
+    n_floor = scrapy.Field()
